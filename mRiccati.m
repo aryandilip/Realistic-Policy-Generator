b3252518be64dc16098ac52fe3e17b7a9b129ae8
aryandilip <44670813+aryandilip@users.noreply.github.com>
2019-07-19 13:08:31 +0530

    Delete mRiccati.m

--- mRiccati.m
+++
@@ -1,7 +0,0 @@
-function dXdt = mRiccati(t, X, A, B, Q, R)
-[sa, ta] = size(A);
-[sb, tb] = size(B);
-
-X = reshape(X, sa,sa); %Convert from "n^2"-by-1 to "n"-by-"n"
-dXdt = A.'*X + X*A - X*(B*(R^-1)*B.')*X + Q; %Determine derivative
-dXdt = dXdt(:); %Convert from "n"-by-"n" to "n^2"-by-1
